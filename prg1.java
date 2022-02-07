6ad64d0b83d31cd0f65a886c95f9f732d04d7211
Prem <170030111@kluniversity.in>
2022-02-07 12:45:49 +0530

    practice p1

---
+++ prg1.java
@@ -0,0 +1,17 @@
+package Phase1;
+
+public class prg1 {
+	public static void main(String[] args) {
+
+	    
+	    Integer obj = Integer.valueOf("10");
+	    Double obj1 = Double.valueOf("28.36");
+	    int var1 = obj.intValue();
+	    double var2 = obj1.doubleValue();
+	    System.out.println("int variable: " + var1);
+	    System.out.println("double variable: " + var2);
+	    
+	  }
+
+}
+ 
